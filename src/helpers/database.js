f3458809d0f948a12c4bf73d1e751b12951d6933
Everton <evertontomalok123@gmail.com>
2021-01-24 02:50:54 -0300

    first tests

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -2,11 +2,19 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const HOST = process.env.DB_HOST || "localhost";
-const PORT = process.env.DB_PORT || 5432;
-const PASS = process.env.DB_PW;
-const USER = process.env.DB_USER;
-const DATABASE = process.env.DB_NAME;
+if (process.env.TESTING === "false") {
+  const HOST = process.env.DB_HOST || "localhost";
+  const PORT = process.env.DB_PORT || 5432;
+  const PASS = process.env.DB_PW ;
+  const USER = process.env.DB_USER;
+  const DATABASE = process.env.DB_NAME;
+} else {
+  const HOST = "localhost";
+  const PORT = 5432;
+  const PASS = "my_user";
+  const USER = "my_pass";
+  const DATABASE = "tests";
+}
 
 const sequelize = new Sequelize(
   `postgres://${USER}:${PASS}@${HOST}:${PORT}/${DATABASE}`,
