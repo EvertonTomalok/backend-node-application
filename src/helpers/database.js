878621b5ceb89d0a61612a3f5efb237cb143ffb4
Everton Tomalok <evertontomalok123@gmail.com>
2021-05-19 11:33:58 -0300

    feat: migrate production db to digital ocean

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,14 +3,18 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const LOGGING = process.env.LOGGING !== "false";
-const DATABASE = process.env.TESTING === "true" ? "postgres" : "bossa_box_backend";
+const DATABASE = process.env.TESTING === "true" ? "postgres" : "backend";
 const HOST = process.env.DB_HOST || "localhost";
 const PORT = process.env.DB_PORT || 5432;
-const PASS = process.env.DB_PW || "hZmc8cQBTZM";
-const USER = process.env.DB_USER || "postgres";
+const PASS = process.env.DB_PW || "my_pass";
+const USER = process.env.DB_USER || "my_user";
 
 const sequelize = new Sequelize(
   `postgres://${USER}:${PASS}@${HOST}:${PORT}/${DATABASE}`,
+  {
+    dialect: "postgres",
+    ssl: HOST !== "localhost",
+  },
   { logging: LOGGING },
 );
 
