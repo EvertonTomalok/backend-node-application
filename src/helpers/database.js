1676e04b9b53e266c1bfcd465c4156359dca4981
Everton Tomalok <evertontomalok123@gmail.com>
2021-05-18 23:34:48 -0300

    Minor/testing (#3)
    * chore: testing
    
    * fix: cd

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const LOGGING = process.env.LOGGING !== "false";
-const DATABASE = process.env.TESTING === "true" ? "postgres" : process.env.DB_NAME;
+const DATABASE = process.env.TESTING === "true" ? "postgres" : "bossa_box_backend";
 const HOST = process.env.DB_HOST || "localhost";
 const PORT = process.env.DB_PORT || 5432;
-const PASS = process.env.DB_PW;
-const USER = process.env.DB_USER;
+const PASS = process.env.DB_PW || "hZmc8cQBTZM";
+const USER = process.env.DB_USER || "postgres";
 
 const sequelize = new Sequelize(
   `postgres://${USER}:${PASS}@${HOST}:${PORT}/${DATABASE}`,
