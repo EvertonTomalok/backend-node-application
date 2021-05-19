94940bf890d85e2e56488a8ead34be5da18edcdd
Everton Tomalok <evertontomalok123@gmail.com>
2021-05-19 11:52:29 -0300

    fix: database connection

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -2,16 +2,25 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const LOGGING = process.env.LOGGING !== "false";
 const DATABASE = process.env.TESTING === "true" ? "postgres" : "backend";
 const HOST = process.env.DB_HOST || "localhost";
 const PORT = process.env.DB_PORT || 5432;
 const PASS = process.env.DB_PW || "my_pass";
 const USER = process.env.DB_USER || "my_user";
 
-const sequelize = new Sequelize(
-  `postgres://${USER}:${PASS}@${HOST}:${PORT}/${DATABASE}?ssl=${HOST !== "localhost"}`,
-  { logging: LOGGING },
-);
+const sequelize = new Sequelize({
+  database: DATABASE,
+  username: USER,
+  password: PASS,
+  host: HOST,
+  port: PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
